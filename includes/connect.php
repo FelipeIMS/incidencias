4757c705c1d921c1632d7db7923ffc59e268285d
luis yanez <luis.yanez@clinicalircay.cl>
2022-02-08 15:47:26 -0300

    eliminar db_connect

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost = "localhost"; //Host
-$dbuser = "abbas0v"; //Database user
-$dbpass = "123"; //Database password
-$dbname = "phpmultiuserlogin"; //Database name
+$dbhost = "192.168.2.175:3306"; //Host
+$dbuser = "username"; //Database user
+$dbpass = "password"; //Database password
+$dbname = "incidencias_s"; //Database name
 $conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
 mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
 ?>
