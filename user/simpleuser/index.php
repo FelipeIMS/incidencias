3f3227a38fe234cfb19a2b1ef7acade3808a636e
FelipeIMS <felipemendezsilv@gmail.com>
2022-02-08 18:30:53 -0300

    cambios
    index: bootstrap actualizado
    simple user: cambio de idioma

--- user/simpleuser/index.php
+++ user/simpleuser/index.php
@@ -4,10 +4,18 @@
   <head>
     <meta charset="utf-8">
     <title>SIMPLE USER</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css">
   </head>
   <body>
     <h1>This is User page</h1>
-    <h2>Hello: <?php $ufunc->UserName(); //Show name who is in session user?></h2>
-    <a href="../../includes/logout.php">Logout</a>
+    <h2>Hola: <?php $ufunc->UserName(); //Show name who is in session user?></h2>
+
+
+
+
+    <a class="btn btn-primary" href="../../includes/logout.php">Logout</a>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
   </body>
 </html>
