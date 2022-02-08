742f4bdcd230cc6deb5262e3303e9f09d5cc66e3
luis yanez <luis.yanez@clinicalircay.cl>
2022-02-08 15:36:34 -0300

    cdn admin

--- user/admin/index.php
+++ user/admin/index.php
@@ -1,13 +1,29 @@
-<?php include 'settings.php'; //include settings ?>
+<?php include 'settings.php'; //include settings 
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>ADMIN</title>
-  </head>
-  <body>
-    <h1>This is Admin page</h1>
-    <h2>Hello: <?php $ufunc->UserName(); //Show name who is in session user?></h2>
-    <a href="../../includes/logout.php">Logout</a>
-  </body>
+
+<head>
+  <meta charset="utf-8">
+  <title>ADMIN</title>
+  <!-- Font Awesome -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+  <!-- MDB -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css" rel="stylesheet" />
+</head>
+
+<body>
+  <h1>This is Admin page</h1>
+  <h2>Hello: <?php $ufunc->UserName(); //Show name who is in session user
+              ?></h2>
+  <a href="../../includes/logout.php">Logout</a>
+  <!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"
+></script>
+</body>
+
 </html>
