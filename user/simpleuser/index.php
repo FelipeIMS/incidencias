0c6e0f37a0862f9b398b0782abf99a2746c37a62
FelipeIMS <felipemendezsilv@gmail.com>
2022-02-10 16:29:40 -0300

    actualizar index

--- user/simpleuser/index.php
+++ user/simpleuser/index.php
@@ -31,53 +31,57 @@
 		
   </head>
   <body>
-    <h1>This is User page</h1>
-    <h2>Hola: <?php $ufunc->UserName(); //Show name who is in session user?></h2>
+	  <header class= "header__content">
+		  <h1>This is User page, Hola: <?php $ufunc->UserName(); //Show name who is in session user?></h1>
 
-    <form id="combo" name="combo" action="guarda.php" method="POST">
-			<div>Selecciona Servicio : <select name="combo_servicio" id="combo_servicio">
-				<option value="0">Seleccionar Estado</option>
-				<?php while($row = $resultado->fetch_assoc()) { ?>
-					<option value="<?php echo $row['id']; ?>"><?php echo $row['nombres']; ?></option>
-				<?php } ?>
-			</select></div>
-			
-			<br />
-			
-			<div>Selecciona Tipo de Servicio : <select name="combo_ts" id="combo_ts"></select></div>
-			
-			<br />
-			<div class="form-outline mb-4 w-25">
-				<input type="text" id="form1Example1" class="form-control" />
-				<label class="form-label" for="form1Example1">Email address</label>
-			</div>
+	  </header>
+	<center>
+		<form id="combo" name="combo" action="guarda.php" method="POST">
+				<div>Selecciona Servicio : <select name="combo_servicio" id="combo_servicio">
+					<option value="0">Seleccionar Estado</option>
+					<?php while($row = $resultado->fetch_assoc()) { ?>
+						<option value="<?php echo $row['id']; ?>"><?php echo $row['nombres']; ?></option>
+					<?php } ?>
+				</select></div>
+				
+				<br />
+				
+				<div>Selecciona Tipo de Servicio : <select name="combo_ts" id="combo_ts"></select></div>
+				
+				<br />
+				<div class="form-outline mb-4 w-25">
+					<input type="text" id="form1Example1" class="form-control" />
+					<label class="form-label" for="form1Example1">Email address</label>
+				</div>
+	
+				<!-- Password input -->
+				<div class="form-outline mb-4 w-25">
+					<input type="text" id="form1Example2" class="form-control" />
+					<label class="form-label" for="form1Example2">Password</label>
+				</div>
+				<div class="form-outline mb-4 w-25">
+					<input type="text" id="form1Example2" class="form-control" />
+					<label class="form-label" for="form1Example2">Password</label>
+				</div>
+				<div class="form-outline mb-4 w-25 ">
+					<textarea class="form-control" id="textAreaExample" style= "resize: none;" rows="4"></textarea>
+					<label class="form-label" for="textAreaExample">Message</label>
+				</div>
+	
+				<input type="submit" id="enviar" name="enviar" value="Guardar" />
+			</form>
 
-			<!-- Password input -->
-			<div class="form-outline mb-4 w-25">
-				<input type="text" id="form1Example2" class="form-control" />
-				<label class="form-label" for="form1Example2">Password</label>
-			</div>
-			<div class="form-outline mb-4 w-25">
-				<input type="text" id="form1Example2" class="form-control" />
-				<label class="form-label" for="form1Example2">Password</label>
-			</div>
-			<div class="form-outline">
-				<textarea class="form-control" id="textAreaExample"></textarea>
-				<label class="form-label" for="textAreaExample">Message</label>
-			</div>
-
-			<input type="submit" id="enviar" name="enviar" value="Guardar" />
-		</form>
+			<a class="btn btn-primary" href="../../includes/logout.php">Logout</a>
+			<script
+				type="text/javascript"
+				src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"
+			></script>
+	</center>
 
     
 
 
 
 
-    <a class="btn btn-primary" href="../../includes/logout.php">Logout</a>
-	<script
-		type="text/javascript"
-		src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"
-></script>
   </body>
 </html>
