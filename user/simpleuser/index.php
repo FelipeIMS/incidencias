4337b37322a7a7ed4814900ff64282b112b82830
FelipeIMS <felipemendezsilv@gmail.com>
2022-02-10 17:53:23 -0300

    bla

--- user/simpleuser/index.php
+++ user/simpleuser/index.php
@@ -37,51 +37,50 @@
 	  </header>
 	<center>
 		<form id="combo" name="combo" action="guarda.php" method="POST">
-				<div>Selecciona Servicio : <select name="combo_servicio" id="combo_servicio">
-					<option value="0">Seleccionar Estado</option>
-					<?php while($row = $resultado->fetch_assoc()) { ?>
-						<option value="<?php echo $row['id']; ?>"><?php echo $row['nombres']; ?></option>
-					<?php } ?>
-				</select></div>
-				
-				<br />
-				
-				<div>Selecciona Tipo de Servicio : <select name="combo_ts" id="combo_ts"></select></div>
-				
-				<br />
-				<div class="form-outline mb-4 w-25">
-					<input type="text" id="form1Example1" class="form-control" />
-					<label class="form-label" for="form1Example1">Email address</label>
-				</div>
-	
-				<!-- Password input -->
-				<div class="form-outline mb-4 w-25">
-					<input type="text" id="form1Example2" class="form-control" />
-					<label class="form-label" for="form1Example2">Password</label>
-				</div>
-				<div class="form-outline mb-4 w-25">
-					<input type="text" id="form1Example2" class="form-control" />
-					<label class="form-label" for="form1Example2">Password</label>
-				</div>
-				<div class="form-outline mb-4 w-25 ">
-					<textarea class="form-control" id="textAreaExample" style= "resize: none;" rows="4"></textarea>
-					<label class="form-label" for="textAreaExample">Message</label>
-				</div>
-	
-				<input type="submit" id="enviar" name="enviar" value="Guardar" />
+			<div class="form-outline datetimepicker mb-4 w-25" data-mdb-inline="true" >
+				<input type="text" class="form-control" id="datetimepickerInline" />
+				<label for="datetimepickerInline" class="form-label">
+					Select Date and Time</label
+				>
+			</div>
+			<div>Selecciona Servicio : <select name="combo_servicio" id="combo_servicio">
+				<option value="0">Seleccionar Estado</option>
+				<?php while($row = $resultado->fetch_assoc()) { ?>
+					<option value="<?php echo $row['id']; ?>"><?php echo $row['nombres']; ?></option>
+				<?php } ?>
+			</select></div>
+			
+			<br />
+			
+			<div>Selecciona Tipo de Servicio : <select name="combo_ts" id="combo_ts"></select></div>
+			
+			<br />
+			<div class="form-outline mb-4 w-25 ">
+				<textarea class="form-control" id="textAreaExample" style= "resize: none;" rows="4"></textarea>
+				<label class="form-label" for="textAreaExample">Observacion</label>
+			</div>
+
+			<div class="mb-4 w-25">
+				<button type="button" class="btn btn-success">Finalizar incidencia</button>
+			</div>
+
+
+			<input type="submit" class="btn btn-info" id="enviar" name="enviar" value="Guardar" />
 			</form>
+			<br />
+
 
 			<a class="btn btn-primary" href="../../includes/logout.php">Logout</a>
-			<script
-				type="text/javascript"
-				src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"
-			></script>
-	</center>
-
-    
-
-
-
-
+		</center>
+		
+		
+		
+		
+		
+		
+	<script
+		type="text/javascript"
+		src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"
+	></script>
   </body>
 </html>
