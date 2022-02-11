5b4ecca79401682a6a097c40a39d6ab145992208
luissssssssssssssxd <98342890+luissssssssssssssxd@users.noreply.github.com>
2022-02-10 22:27:08 -0300

    validarinsert

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php
-$dbhost = "192.168.2.175:3306"; //Host
-$dbuser = "username"; //Database user
-$dbpass = "password"; //Database password
+$dbhost = "localhost"; //Host
+$dbuser = "root"; //Database user
+$dbpass = "2497"; //Database password
 $dbname = "incidencias_s"; //Database name
 $conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
 mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
--- user/admin/insert.php
+++ user/admin/insert.php
@@ -11,34 +11,9 @@ if (!empty($_POST)) {
      VALUES('$nombrets')
     ";
      if (mysqli_query($conn, $query)) {
-          $output .= '<label class="text-success">Insert OK</label>';
-          $select_query = "select * from incidencias i
-     inner join servicios s on  i.id_servicio_1=s.id
-     inner join tipo_servicio tp on  tp.id = s.id_tipoServicio_1
-     inner join area a on i.id_area_1= a.id";
-          $result = mysqli_query($conn, $select_query);
-          $output .= '
-      <table class="table table-bordered">  
-                    <tr>  
-                        <th>ID</th>
-            <th>Servicio</th>
-            <th>Tipo</th>
-            <th>Ver</th>
-                    </tr>
-
-     ';
-          while ($row = mysqli_fetch_array($result)) {
-               $output .= '
-       <tr>  
-       <td>' . $row["id"] . '</td> 
-       <td>' . $row["nombres"] . '</td>  
-                         <td>' . $row["nombrets"] . '</td>  
-                         <td><input type="button" name="Ver" value="Ver" id="' . $row["id"] . '" class="btn btn-info btn-xs view_data" /></td>  
-                    </tr>
-      ';
-          }
-          $output .= '</table>';
-     }
+        
+      
      echo $output;
+}
 }
 ?>
