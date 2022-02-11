759b644a48eb7eb897f3f595ff1ab4783eae73d3
FelipeIMS <felipemendezsilv@gmail.com>
2022-02-11 10:48:16 -0300

    change

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,13 @@
 <?php
-$dbhost = "localhost"; //Host
-$dbuser = "root"; //Database user
-$dbpass = "2497"; //Database password
+// $dbhost = "localhost"; //Host
+// $dbuser = "root"; //Database user
+// $dbpass = "2497"; //Database password
+// $dbname = "incidencias_s"; //Database name
+// $conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
+// mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
+$dbhost = "192.168.2.175:3306"; //Host
+$dbuser = "username"; //Database user
+$dbpass = "password"; //Database password
 $dbname = "incidencias_s"; //Database name
 $conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
 mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
