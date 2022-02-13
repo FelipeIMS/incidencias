a68c652e772a4b4435afcd98dd8221306090380b
FelipeIMS <felipemendezsilv@gmail.com>
2022-02-13 13:27:28 -0300

    s

--- user/simpleuser/guarda.php
+++ user/simpleuser/guarda.php
@@ -14,5 +14,4 @@ if($resultado){
 	} else {
 	echo "Error al Registrar";
 }
-
 ?>
--- user/simpleuser/index.php
+++ user/simpleuser/index.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css">
 	<link rel="stylesheet" href="css/style.css">
-    <script languaje="javascript" src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script language="javascript" src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 		
 		<script language="javascript">
 			$(document).ready(function(){
