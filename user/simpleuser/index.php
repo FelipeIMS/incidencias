e8868d5c04b19af28fe8407fb0f3bcc63f188b18
FelipeIMS <felipemendezsilv@gmail.com>
2022-02-13 16:02:32 -0300

    pruebas

--- user/simpleuser/index.php
+++ user/simpleuser/index.php
@@ -13,7 +13,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css">
-	<link rel="stylesheet" href="css/style.css">
     <script language="javascript" src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 		
 		<script language="javascript">
@@ -38,37 +37,50 @@
 		  <h1>This is User page, Hola: <?php $ufunc->UserName(); //Show name who is in session user?></h1>
 
 	  </header>
-		<form id="combo" name="combo" action="guarda.php" method="POST">
-			
-			<div class="mt-3">Selecciona Servicio : <select name="combo_servicio" id="combo_servicio">
-				<option value="0">Seleccionar Servicio</option>
-				<?php while($row = $resultado->fetch_assoc()) { ?>
-					<option value="<?php echo $row['id']; ?>"><?php echo $row['nombres']; ?></option>
-				<?php } ?>
-			</select></div>
-			
-			<div class="mt-3 mb-3">Selecciona Tipo de Servicio : <select name="combo_ts" id="combo_ts"></select></div>
-			
-			<div>Selecciona Area : <select name="select_area" id="select_area">
-			<option value="0">Seleccionar Area</option>
-				<?php while($row = $resultado2->fetch_assoc()) { ?>
-					<option value="<?php echo $row['id']; ?>"><?php echo $row['nombreArea']; ?></option>
-				<?php } ?>
-			</select></div>
-			<div class="form-outline mt-3 mb-3 w-25">
-				<input type="text" id="fechaF" name="fechaFin" class="form-control" />
-				<label class="form-label" for="fechaF">Fecha Fin</label>
-			</div>
+	  <div class="container">
 
-			<div class="form-outline mb-3 w-25 ">
-				<textarea class="form-control" id="obs" name="obs" style= "resize: none;" rows="4"></textarea>
-				<label class="form-label" for="obs">Observacion</label>
-			</div>
-			<button type="button" class="mb-3 btn btn-success">Finalizar incidencia</button>
+		  <div class="col-md-12">
+			  <div class="card text-center">
+				<div class="card-header">Registrar Incidencia</div>
+				<div class="card-body">
+					<form id="combo" name="combo" action="guarda.php" method="POST">
+						
+						<div class="mt-3">Selecciona Servicio : <select name="combo_servicio" id="combo_servicio">
+							<option value="0">Seleccionar Servicio</option>
+							<?php while($row = $resultado->fetch_assoc()) { ?>
+								<option value="<?php echo $row['id']; ?>"><?php echo $row['nombres']; ?></option>
+							<?php } ?>
+						</select></div>
+						
+						<div class="mt-3 mb-3">Selecciona Tipo de Servicio : <select name="combo_ts" id="combo_ts"></select></div>
+						
+						<div>Selecciona Area : <select name="select_area" id="select_area">
+						<option value="0">Seleccionar Area</option>
+							<?php while($row = $resultado2->fetch_assoc()) { ?>
+								<option value="<?php echo $row['id']; ?>"><?php echo $row['nombreArea']; ?></option>
+							<?php } ?>
+						</select></div>
+						<div class="form-outline mt-3 mb-3 w-50" style="margin: 0 auto;">
+							<input type="text" id="fechaF" name="fechaFin" class="form-control" />
+							<label class="form-label" for="fechaF">Fecha Fin</label>
+						</div>
 			
-			<input type="submit" class="btn btn-info" id="enviar" name="enviar" value="Guardar" />
-		</form>
+						<div class="form-outline mb-3 w-50" style="margin: 0 auto;">
+							<textarea class="form-control" id="obs" name="obs" style= "resize: none;" rows="4"></textarea>
+							<label class="form-label" for="obs">Observacion</label>
+						</div>
+						<button type="button" class="btn btn-success">Finalizar incidencia</button>
+						
+					</form>
+					
+				</div>
+				<div class="card-footer text-muted">
+					<input type="submit" class="btn btn-info" id="enviar" name="enviar" value="Guardar" />
+				</div>
+			</div>
+		  </div>
 			<a class="btn btn-primary" href="../../includes/logout.php">Logout</a>
+	  </div>
 		
 		
 		
