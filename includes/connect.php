9c77ad2c7dc43e280ba06c9034e6fec4784f3466
luissssssssssssssxd <98342890+luissssssssssssssxd@users.noreply.github.com>
2022-02-15 00:05:13 -0300

    validar_insert

--- includes/connect.php
+++ includes/connect.php
@@ -1,15 +1,15 @@
 <?php
-// $dbhost = "localhost"; //Host
-// $dbuser = "root"; //Database user
-// $dbpass = "2497"; //Database password
-// $dbname = "incidencias_s"; //Database name
-// $conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
+$dbhost = "localhost"; //Host
+$dbuser = "root"; //Database user
+$dbpass = "2497"; //Database password
+$dbname = "incidencias_s"; //Database name
+$conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
 // mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
-$dbhost = "192.168.2.175:3306"; //Host
+/* $dbhost = "192.168.2.175:3306"; //Host
 $dbuser = "username"; //Database user
 $dbpass = "password"; //Database password
 $dbname = "incidencias_s"; //Database name
-$conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
+$conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection */
 mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
 // $dbhost = "localhost"; //Host
 // $dbuser = "root"; //Database user
@@ -17,4 +17,3 @@ mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
 // $dbname = "incidencias_s"; //Database name
 // $conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
 // mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
-?>
