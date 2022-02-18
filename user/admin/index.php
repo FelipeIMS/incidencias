5dab7b7204b207a1a5593c8d02ffe2ffedff2a62
luissssssssssssssxd <98342890+luissssssssssssssxd@users.noreply.github.com>
2022-02-18 01:36:05 -0300

    registeruser(MD5)

--- user/admin/index.php
+++ user/admin/index.php
@@ -26,24 +26,30 @@ while ($r = $query2->fetch_object()) { // Recorrer los resultados de Ejecutar la
 
 <head>
   <title>CRUD INCIDENCIAS</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <!-- CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script src="./chart.min.js"></script>
 
 
 </head>
 
 <body>
-  <br /><br />
-  <div class="container" style="width:700px;">
+
+  <div class="container">
     <h3 align="center">Incidencias Clinica Lircay</h3>
     <br />
+    <div class="container">
+      <button type="button" name="age" id="age" data-toggle="modal" data-target="#add_data_Modal" class="btn btn-success ">Registrar Tipo Servicio</button>
+      <button type="button" name="age" id="age" data-toggle="modal" data-target="#add_data_Modal2" class="btn btn-success">Registrar Servicio</button>
+      <button type="button" name="age" id="age" data-toggle="modal" data-target="#add_data_Modal3" class="btn btn-success">Registrar Area</button>
+      <button type="button" name="age" id="age" data-toggle="modal" data-target="#add_data_Modal4" class="btn btn-success">Registrar Usuarios</button>
+
+
+    </div>
+
     <div class="table-responsive">
-      <div align="right">
-        <button type="button" name="age" id="age" data-toggle="modal" data-target="#add_data_Modal" class="btn btn-success">Registrar Tipo Servicio</button>
-      </div>
+
+
       <br />
       <div id="employee_table">
         <table class="table table-bordered">
@@ -55,21 +61,30 @@ while ($r = $query2->fetch_object()) { // Recorrer los resultados de Ejecutar la
 
           </tr>
           <?php
-          while ($row = mysqli_fetch_array($result)) {
+          if ($result->num_rows > 0) {
+            while ($row = mysqli_fetch_array($result)) {
           ?>
-            <tr>
-              <td><?php echo $row["id"]; ?></td>
-              <td><?php echo $row["nombreservicio"]; ?></td>
-              <td><?php echo $row["nombretipo"]; ?></td>
-              <td><input type="button" name="view" value="Ver" id="<?php echo $row["id"]; ?>" class="btn btn-info btn-xs view_data" /></td>
-            </tr>
+              <tr>
+                <td><?php echo $row["id"]; ?></td>
+                <td><?php echo $row["nombreservicio"]; ?></td>
+                <td><?php echo $row["nombretipo"]; ?></td>
+                <td><input type="button" name="view" value="Ver" id="<?php echo $row["id"]; ?>" class="btn btn-info btn-xs view_data" /></td>
+              </tr>
           <?php
+            }
+          } else {
+            echo '<tr>';
+            echo '<td class="text-center" colspan=3>No se ha encontrado ningun registro</td>';
+            echo '</tr>';
           }
+
+
           ?>
         </table>
       </div>
     </div>
   </div>
+  <!-- GRAFICOS -->
   <div class="container">
     <h1 class="text-center">Incidencias por mes</h1>
     <canvas id="chart1" style="width:100%;" height="100"></canvas>
@@ -81,7 +96,7 @@ while ($r = $query2->fetch_object()) { // Recorrer los resultados de Ejecutar la
           <?php endforeach; ?>
         ],
         datasets: [{
-          
+
           label: ' Cantidad de Incidencias por mes',
           data: [
             <?php foreach ($data as $d) : ?>
@@ -91,12 +106,12 @@ while ($r = $query2->fetch_object()) { // Recorrer los resultados de Ejecutar la
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(255, 159, 64, 0.2)',
-           
+
           ],
           borderColor: [
             'rgb(255, 99, 132)',
             'rgb(255, 159, 64)',
-          
+
           ],
           borderWidth: 2
         }]
@@ -122,6 +137,7 @@ while ($r = $query2->fetch_object()) { // Recorrer los resultados de Ejecutar la
 
 </html>
 
+<!--  MODAL TIPO SERVICIOS -->
 <div id="add_data_Modal" class="modal fade">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -160,6 +176,85 @@ while ($r = $query2->fetch_object()) { // Recorrer los resultados de Ejecutar la
     </div>
   </div>
 </div>
+<!-- MODAL SERVICIOS -->
+<div id="add_data_Modal2" class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Registro Servicio</h4>
+      </div>
+      <div class="modal-body">
+        <form method="post" id="insert_form2">
+          <label>Servicio</label>
+          <input type="text" name="nombres" id="nombres" class="form-control" />
+          <br />
+          <input type="submit" name="insert" id="insert" value="Guardar" class="btn btn-success" style="margin-top: 20px;" />
+
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Salir</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- MODAL AREAS -->
+<div id="add_data_Modal3" class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Registro Areas</h4>
+      </div>
+      <div class="modal-body">
+        <form method="post" id="insert_form3">
+          <label>Area</label>
+          <input type="text" name="nombrearea" id="nombrearea" class="form-control" />
+          <br />
+          <input type="submit" name="insert" id="insert" value="Guardar" class="btn btn-success" style="margin-top: 20px;" />
+
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Salir</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- MODAL USERS -->
+<div id="add_data_Modal4" class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Registro Usuarios</h4>
+      </div>
+      <div class="modal-body">
+        <form method="post" id="insert_form4">
+          <label>Nombre</label>
+          <input type="text" name="name" id="name" class="form-control" />
+          <br />
+          <label>Email</label>
+          <input type="text" name="login" id="login" class="form-control" />
+          <br />
+          <label>Contraseña</label>
+          <input type="text" name="password" id="password" class="form-control" />
+          <br />
+          <select class="form-control" id="rol" name="rol" >
+            <option selected>Seleccionar Rol</option>
+            <option value="1">Administrador</option>
+            <option value="0">Usuario</option>
+          </select>
+          <br />
+          <input type="submit" name="insert" id="insert" value="Guardar" class="btn btn-success" style="margin-top: 20px;" />
+
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Salir</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <div id="dataModal" class="modal fade">
   <div class="modal-dialog">
@@ -176,39 +271,109 @@ while ($r = $query2->fetch_object()) { // Recorrer los resultados de Ejecutar la
     </div>
   </div>
 </div>
+<!-- js -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script>
   $(document).ready(function() {
     $('#insert_form').on("submit", function(event) {
-      event.preventDefault();
-      if ($('#nombrets').val() == "") {
-        alert("Name is required");
-      } else if ($('#address').val() == '') {
-        alert("Address is required");
-      } else if ($('#designation').val() == '') {
-        alert("Designation is required");
-      } else {
-        $.ajax({
-          url: "insert.php",
-          method: "POST",
-          data: $('#insert_form').serialize(),
-          beforeSend: function() {
-            $('#insert').val("Inserting");
+        event.preventDefault();
+        if ($('#nombrets').val() == "") {
+          alert("Name is required");
+        } else if ($('#address').val() == '') {
+          alert("Address is required");
+        } else if ($('#designation').val() == '') {
+          alert("Designation is required");
+        } else {
+          $.ajax({
+            url: "insert.php",
+            method: "POST",
+            data: $('#insert_form').serialize(),
+            beforeSend: function() {
+              $('#insert').val("Inserting");
 
-          },
-          success: function(data) {
-            $('#insert_form')[0].reset();
-            $('#add_data_Modal').modal('hide');
-            $('#employee_table').html(data);
+            },
+            success: function(data) {
+              $('#insert_form')[0].reset();
+              $('#add_data_Modal').modal('hide');
+              $('#employee_table').html(data);
 
-          }
-        });
-      }
-    });
+            }
+          });
+        }
+      }),
+      $('#insert_form2').on("submit", function(event) {
+        event.preventDefault();
+        if ($('#nombres').val() == "") {
+          alert("prueba is required");
 
+        } else {
+          $.ajax({
+            url: "insert_servicio.php",
+            method: "POST",
+            data: $('#insert_form2').serialize(),
+            beforeSend: function() {
+              $('#insert').val("Insertando");
 
+            },
+            success: function(data) {
+              $('#insert_form2')[0].reset();
+              $('#add_data_Modal2').modal('hide');
+              $('#employee_table').html(data);
 
+            }
+          });
+        }
+      }),
+      $('#insert_form3').on("submit", function(event) {
+        event.preventDefault();
+        if ($('#nombrearea').val() == "") {
+          alert("Area es requerido");
 
+        } else {
+          $.ajax({
+            url: "insert_area.php",
+            method: "POST",
+            data: $('#insert_form3').serialize(),
+            beforeSend: function() {
+              $('#insert').val("Insertando");
+
+            },
+            success: function(data) {
+              $('#insert_form3')[0].reset();
+              $('#add_data_Modal3').modal('hide');
+              $('#employee_table').html(data);
+
+            }
+          });
+        }
+      })
+      ,
+      $('#insert_form4').on("submit", function(event) {
+        event.preventDefault();
+        if ($('#email').val() == "") {
+          alert("Area es requerido");
+
+        } else {
+          $.ajax({
+            url: "insert_user.php",
+            method: "POST",
+            data: $('#insert_form4').serialize(),
+            beforeSend: function() {
+              $('#insert').val("Insertando");
+
+            },
+            success: function(data) {
+              $('#insert_form4')[0].reset();
+              $('#add_data_Modal4').modal('hide');
+              $('#employee_table').html(data);
+
+            }
+          });
+        }
+      });
     $(document).on('click', '.view_data', function() {
       //$('#dataModal').modal();
       var id = $(this).attr("id");
---
+++ user/admin/insert_area.php
@@ -0,0 +1,66 @@
+<?php include 'settings.php'; //include settings 
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+   <script src="./chart.min.js"></script>
+
+
+</head>
+
+<body>
+   <?php
+
+   if (!empty($_POST)) {
+      $nombrearea = mysqli_real_escape_string($conn, $_POST["nombrearea"]);
+      $area = mysqli_query($conn, "SELECT * FROM area WHERE nombreArea = '$nombrearea'");
+
+      if (mysqli_num_rows($area) > 0) {
+         echo "<script>  Swal.fire({
+          position: 'center',
+          icon: 'warning',
+          title: 'Error, dato ya existe',
+          showConfirmButton: false,
+          timer: 1500
+        });</script>";
+         echo '<script type="text/JavaScript"> setTimeout(function(){
+         window.location.reload(1);
+      }, 3000); </script>';
+      } else {
+         $query = "
+          INSERT INTO area(nombreArea)  
+           VALUES('$nombrearea')
+          ";
+         if (mysqli_query($conn, $query)) {
+            echo "<script>  Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: 'Registro exitoso',
+                    showConfirmButton: false,
+                    timer: 3000
+                  });</script>";
+            echo '<script type="text/JavaScript"> setTimeout(function(){
+                    window.location.reload(1);
+                 }, 3000); </script>';
+         } else {
+            echo "Error: " . $sql . "" . mysqli_error($conn);
+         }
+         $conn->close();
+      }
+   }
+
+   ?>
+
+   <!-- js -->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body>
+
+
+</html>
