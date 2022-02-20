5cd589026b03d56142eeff6a1d755fb6125ecb5d
FelipeIMS <felipemendezsilv@gmail.com>
2022-02-19 21:49:44 -0300

    pruebas-casa

--- includes/connect.php
+++ includes/connect.php
@@ -5,16 +5,16 @@
 // $dbname = "incidencias_s"; //Database name
 // $conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
 // mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
-$dbhost = "192.168.2.175:3306"; //Host
-$dbuser = "username"; //Database user
-$dbpass = "password"; //Database password
-$dbname = "incidencias_s"; //Database name
+// $dbhost = "192.168.2.175:3306"; //Host
+// $dbuser = "username"; //Database user
+// $dbpass = "password"; //Database password
+// $dbname = "incidencias_s"; //Database name
 // $conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
 // mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
-// $dbhost = "localhost"; //Host
-// $dbuser = "root"; //Database user
-// $dbpass = ""; //Database password
-// $dbname = "incidencias_s"; //Database name
+$dbhost = "localhost"; //Host
+$dbuser = "root"; //Database user
+$dbpass = ""; //Database password
+$dbname = "incidencias_s"; //Database name
 $conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
 mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
 ?>
