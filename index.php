0624b4ec8d26cad082f06dbd9bab55d026881a65
FelipeIMS <felipemendezsilv@gmail.com>
2022-02-21 15:51:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="author" content="Musa Abbasov">
-  <title>Multi-user login with PHP</title>
+  <title>Portal Incidencias</title>
   <!-- <link href="https://getbootstrap.com/docs/4.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
